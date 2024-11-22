ba814ffce59ced730208f0626e59fc227fe034bd
Zerosixxxx <paulmenzel352@gmail.com>
2024-11-22 08:59:06 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,287 @@
+basic.showLeds(`
+    # . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . # . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . # . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . # .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . #
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . # .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    # . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . # . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . # .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . #
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . #
+    . . . . #
+    . . . . #
+    . . . . #
+    . . . . #
+    `)
+basic.showLeds(`
+    # . . . .
+    # . . . .
+    # . . . .
+    # . . . .
+    # . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    # # # # #
+    `)
+basic.showLeds(`
+    # # # # #
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    # # # # #
+    # . . . #
+    # . . . #
+    # . . . #
+    # # # # #
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    . # . # .
+    . # # # .
+    . . . . .
+    `)
+basic.showLeds(`
+    # # # # #
+    # . . . #
+    # . . . #
+    # . . . #
+    # # # # #
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showIcon(IconNames.Meh)
+basic.showLeds(`
+    # # . # #
+    . . . . .
+    . # . . .
+    . . # . .
+    . . . # .
+    `)
+basic.showIcon(IconNames.Scissors)
+basic.showLeds(`
+    # . . # #
+    . # . # #
+    . . # . .
+    . # . # #
+    # . . # #
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showIcon(IconNames.Giraffe)
+basic.showLeds(`
+    . . . . #
+    # # # # #
+    . # . . .
+    # # . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . # . # .
+    . # # # .
+    . . . # .
+    . . . # .
+    . . . # #
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . # #
+    . . . # .
+    # # # # #
+    # . . . .
+    `)
+basic.showLeds(`
+    # # . . .
+    . # . . .
+    . # . . .
+    . # # # .
+    . # . # .
+    `)
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    . . # . .
+    . . # . .
+    . # . # .
+    `)
+basic.showLeds(`
+    . . . . .
+    # . . # .
+    . # # # #
+    # . . # .
+    . . . . .
+    `)
+basic.showLeds(`
+    . # . # .
+    . . # . .
+    . . # . .
+    . # # # .
+    . . # . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . # . . #
+    # # # # .
+    . # . . #
+    . . . . .
+    `)
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    . . # . .
+    . . # . .
+    . # . # .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    # . . . #
+    . # # # .
+    . # . # .
+    `)
+basic.showLeds(`
+    . . . # .
+    . # # . .
+    . . # . .
+    . # # . .
+    . . . # #
+    `)
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . # # # .
+    # . . . #
+    # . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    # . . . #
+    . # # # .
+    . # . # .
+    `)
+music.playTone(262, music.beat(BeatFraction.Whole))
+basic.forever(function () {
+	
+})
